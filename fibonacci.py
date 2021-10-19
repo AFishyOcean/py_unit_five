2ad1505e9babaea736b441b994338f853c1832fe
AFishyOcean <dljhazelh@gmail.com>
2021-10-19 09:57:11 -0400

    worked on fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -4,3 +4,17 @@ def fibonacci(x):
     :param number: The number of Fibonacci terms to return
     :return: A string consisting of a number of terms of the Fibonacci sequence.
     """
+    c = ""
+    a = 0
+    b = 1
+    for x in range(x):
+        c +=str (a + b)+ ""
+        a = b
+        b = c
+    return c
+def main():
+    x = int(input("How many terms would you like?"))
+    fibonacci(x)
+    print(fibonaci(x))
+
+main()
--- multiplication.py
+++ multiplication.py
@@ -2,6 +2,14 @@ def multiplication_table(number):
     """
     Ex. multiplication_table(6) returns "6 12 18 24 30 36 42 48 54 60 66 72 "
     :param number: An integer
-    :return: A string of 12 values representing the mulitiplication table of the parameter number.
+    :return: A string of 12 values representing the multiplication table of the parameter number.
     """
-    pass # Make sure to delete this line when writing your function.
+    tot = ""
+    for x in range(12):
+        tot +=str(6 * x + 6)+" "
+    return tot
+def main():
+    number = int(input("What number would you like multiples of?"))
+    print(multiplication_table(number))
+
+main()
